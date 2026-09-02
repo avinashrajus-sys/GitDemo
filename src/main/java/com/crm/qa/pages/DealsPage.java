214fc69e1422052376729af961b89ffe9db0e5cd
avinash <avinashrajus@gmail.com>
2026-09-02 23:32:15 +0530

    Fixed Maven Selenium project

--- src/main/java/com/crm/qa/pages/DealsPage.java
+++ src/main/java/com/crm/qa/pages/DealsPage.java
@@ -1,7 +1,9 @@
 package com.crm.qa.pages;
 
-import com.crm.qa.base.TestBase;
 
-public class DealsPage extends TestBase{
 
-}
+public class DealsPage {
+	public static void main(String[] args) {
+	}
+	}
+
